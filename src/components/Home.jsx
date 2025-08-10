9907b2046d35326e2e4fdebfd7a7e852cff610cf
ZahaSanko001 <zgyro905@gmail.com>
2025-08-10 18:18:58 +0600

    minor adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,18 +17,18 @@ const Home = () => {
             </div>
 
             <div className="flex items-center justify-center mb-40 space-x-8 border-y-2 border-dotted py-4">
-                <Link to={''} className="border-b-2 hover:border-none">CSE</Link>
+                <Link to={'/Notes'} className="border-b-2 hover:border-none">CSE</Link>
+                {/* <p>X</p>
+                <Link to={''} className="border-b-2 hover:border-none"></Link>
                 <p>X</p>
-                <Link to={''} className="border-b-2 hover:border-none">EEE</Link>
-                <p>X</p>
-                <Link to={''} className="border-b-2 hover:border-none">Pharmacy</Link>
+                <Link to={''} className="border-b-2 hover:border-none"></Link> */}
             </div>
 
 
             <div className="w-screen mb-40 flex flex-col md:flex-row gap-8 md:gap-0 px-8 md:px-16">
                 <div className="md:w-[40%]">
                     <p className=" border-x-[12px] w-fit px-4 mb-8 text-xs">About the Archive</p>
-                    <h2 className="w-[80%]">This archive is an idea a few students from 81. It's a collection where later or newer batches can find materials that can help them study on their own pace.</h2>
+                    <h2 className="w-[80%]">This archive is an idea of a few students from 81. It's a collection where later or newer batches can find materials that can help them study on their own pace.</h2>
                 </div>
 
                 <div className="md:w-[60%] h-fit border-l-8 pl-8">
