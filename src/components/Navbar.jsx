03caeb2de5b29224c1ac2cd4cac2ad971d736338
ZahaSanko001 <zgyro905@gmail.com>
2025-08-10 23:24:49 +0600

    changed the theme a bit might change again later with other additions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ const Navbar = () => {
     return (
         <>
             <div className="flex flex-col md:flex-row w-screen justify-around items-center mt-8 mb-40 gap-4 md:gap-0 px-8">
-                <div className="flex w-full md:w-[60%] justify-between space-x-12 items-center">
-                    <a href="/" className="hidden md:block"><img src="logo.png" alt="Stamford" className="border-[1px] rounded-full w-22 h-20 p-1"/></a>
+                <div className="flex w-full md:w-[60%] space-x-12 items-center">
+                    <a href="/" className="hidden md:flex"><img src="logo.png" alt="Freedom" className="rounded-full border-[1px] p-1 w-20"/></a>
                     <div className="flex flex-col w-full text-center">
                         <h1 className="border-b-2 border-dotted text-4xl p-0.5 mb-2"><a href="/" className="hover:border-b-2">STUDY-ARCHIVE</a></h1>
-                        <h2 className="">Notes and metarials by those who already finished it</h2>
+                        <h2 className="">Notes and materials by those who already finished it</h2>
                     </div>
                 </div>
                 
